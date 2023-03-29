1cb6d2249d9d49bf2d90838780f4228257b13592
Alezandro C.R <79446433+zandrocr@users.noreply.github.com>
2023-03-29 14:38:05 -0300

    updata

--- src/views/user/UserView.vue
+++ src/views/user/UserView.vue
@@ -6,9 +6,25 @@
 			data-icon
 			:src="user ? user.photoURL : 'https://img.icons8.com/bubbles/256/google-logo.png'"
 			alt="google" />
-		<div v-show="option">
-			<button v-if="!user" @click="connectGoggle">conect</button>
-			<button v-if="user" @click="logoutGoggle">Logout</button>
+		<div
+			v-if="option"
+			data-userOptions
+			class="col-10 col-md-4 col-lg-3 d-flex flex-column align-items-center justify-content-between text-center">
+			<button @click="options" data-buttons="closeUser">X</button>
+			<div class="connected d-flex flex-column align-items-center justify-content-around">
+				<div v-if="user">
+					<h6>Trainer {{ user.displayName }}</h6>
+					<p>connected with {{ user.email }}</p>
+				</div>
+				<h3 v-if="user">Region not yet designated</h3>
+				<button data-buttons v-if="user" @click="logoutGoggle">Logout</button>
+			</div>
+			<div
+				v-if="!user"
+				class="connect d-flex flex-column align-items-center justify-content-around">
+				<h4>Connect your account to catch your pokemons</h4>
+				<button data-buttons v-if="!user && option" @click="connectGoggle">Conect</button>
+			</div>
 		</div>
 		<h3>{{ user ? user.displayName : "Pokemon Trainer" }}</h3>
 		<button @click="submitPokemon">Submit</button>
@@ -18,15 +34,17 @@
 				v-for="(poke, i) in catchPokemon"
 				:key="i"
 				:data-cardpoke="`${typePokemon(poke.data.types)}`"
-				class="userPokemon col-10 col-md-4 col-lg-3 d-flex flex-column align-items-center">
-				<!-- <div class="pokemonsUser d-flex flex-column align-items-center"> -->
-					<img
-						data-image
-						:src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.data.id}.png`"
-						:alt="poke.data.name"
-						class="col-6" />
-					<h5>{{ poke.data.name }}</h5>
-				<!-- </div> -->
+				class="userPokemon col-10 col-md-4 col-lg-3 d-flex flex-column align-items-center justify-content-around">
+				<div class="deleteContainer col-12 d-flex justify-content-end">
+					<button class="deletePokemon">X</button>
+				</div>
+				<img
+					@click="console.log('e')"
+					data-image
+					:src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.data.id}.png`"
+					:alt="poke.data.name"
+					class="col-5 col-md-6 col-lg-7" />
+				<h6>{{ poke.data.name }}</h6>
 			</div>
 		</div>
 	</section>
@@ -62,6 +80,7 @@
 		},
 		methods: {
 			options() {
+				console.log(this.option)
 				this.option = !this.option
 			},
 			connectGoggle() {
