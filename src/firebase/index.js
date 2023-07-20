4600388a485f953c25652a8e08b3da8d962847ab
Alezandro C.R <79446433+zandrocr@users.noreply.github.com>
2023-07-20 13:00:54 -0300

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-	
+	apiKey: "AIzaSyCfptfii7PxfFFMb3tBNuSnTpk9RTm-PYI",
+	authDomain: "pokevue-7556b.firebaseapp.com",
+	projectId: "pokevue-7556b",
+	storageBucket: "pokevue-7556b.appspot.com",
+	messagingSenderId: "225175084540",
+	appId: "1:225175084540:web:9abf8b82bfb2ea20bee679",
+	measurementId: "G-9G667FTNZ2",
 }
 
 export const app = initializeApp(firebaseConfig)
